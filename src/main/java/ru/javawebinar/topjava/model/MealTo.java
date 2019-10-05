886159841948e3c692a95043e01f105d094a47c4
censor <KuznetsovKD@gmail.com>
2019-10-05 14:25:20 +0300

    1 0 fix

--- src/main/java/ru/javawebinar/topjava/model/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -9,8 +9,6 @@ public class MealTo {
 
     private final int calories;
 
-//    private final Supplier<Boolean> excess;
-//    private final AtomicBoolean excess;
     private final boolean excess;
 
     public MealTo(LocalDateTime dateTime, String description, int calories, boolean excess) {
